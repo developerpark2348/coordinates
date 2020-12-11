066998fb33d00afecdf210ac99f2355ad4fedf01
developerpark2348 <developerpark2348@gmail.com>
2020-12-11 18:36:23 +0900

    refactoring
    refactoring

--- coordinates/main.js
+++ coordinates/main.js
@@ -2,17 +2,18 @@ const vertical = document.querySelector('.vertical');
 const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfwidth = targetRect.width / 2;
+const targetHalfheight = targetRect.height / 2;
+
 
 document.addEventListener('mousemove', function(event) {
     const x = event.clientX
     const y = event.clientY
-    console.log(`${x} ${y}`);
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-    tag.innerHTML = `${x}px, ${y}px`
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x-targetHalfwidth}px, ${y-targetHalfheight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px. ${y}px`;
 });
